469493c61a3d045e472b884f4f8b157ad18abcf5
kazilop <rybka.artem@gmail.com>
2022-07-20 12:49:11 +0300

    update worldgeneration script

--- Assets/Scripts/WorldGenerator/WorldGeneration.cs
+++ Assets/Scripts/WorldGenerator/WorldGeneration.cs
@@ -50,16 +50,40 @@ public class WorldGeneration : MonoBehaviour
 
     private void SpawnNewChunk()
     {
+        int randomIndex = Random.Range(0, chunkPrefab.Count);
 
+        Chunk chunk = null;
+
+        if (!chunk)
+        {
+            GameObject go = Instantiate(chunkPrefab[randomIndex], transform);
+            chunk = go.GetComponent<Chunk>();
+        }
+
+        chunk.transform.position = new Vector3(0, 0, chunkSpawnZ);
+        chunkSpawnZ += chunk.chunkLength;
+
+        activeChunks.Enqueue(chunk);
+        chunk.ShowChunk();
     }
 
     private void DeleteLastChunk()
     {
-
+        Chunk chunk = activeChunks.Dequeue();
+        chunk.HideChunk();
+        chunkPool.Add(chunk);
     }
 
     public void ResetWorld()
     {
+        chunkSpawnZ = firstChunkSpawnPosition;
 
+        for(int i = activeChunks.Count; i != 0; i--)
+        {
+            DeleteLastChunk();
+        }
+
+        for (int i = 0; i < chunkOnScreen; i++)
+            SpawnNewChunk();
     }
 }
